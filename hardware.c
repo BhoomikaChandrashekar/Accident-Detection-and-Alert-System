df6ddf2cf8f6f84821796f682afd2eaf02965a8c
Bhoomika <bhoomi.chandrashekar@gmail.com>
2020-07-30 12:53:50 +0530

    Hardware and Software Code

---
+++ hardware.c
@@ -0,0 +1,196 @@
+#include<SoftwareSerial.h>
+#include "TinyGPS++.h"
+SoftwareSerial GPS_serial(10,11); //GPS connection
+SoftwareSerial GSM_serial(2,3); //GSM connection
+TinyGPSPlus gps;
+//analog pins of Arduino
+int data=A4;
+#define x A1
+#define y A2
+#define z A3
+int xsample=0;
+int ysample=0;
+int zsample=0;
+int start=6;
+int buttonstate=0;
+int flag=0;
+int waitcount=0;
+#define samples 10
+#define MaxVal -10
+int i=0,k=0;
+int gps_status=0;
+float latitude=0;
+float logitude=0;
+int count=0;
+int pulse_count=0;
+int twosec=0;
+void setup()
+{
+	 Serial.begin(9600);
+	 GSM_serial.begin(9600); //GSM baud rate
+	 GPS_serial.begin(9600); //gps baud rate
+	 pinMode(data,INPUT); //heartbeat sensor
+	 pinMode(start,INPUT); //button
+	 pinMode(8, OUTPUT); //For speaker output
+
+	 for(int i=0;i<samples;i++) //accelerometer readings - taking average of 10 readings for accuracy
+	 {
+		 xsample+=analogRead(x);
+		 ysample+=analogRead(y);
+		 zsample+=analogRead(z);
+	 }
+	 xsample/=samples;
+	 ysample/=samples;
+	 zsample/=samples;
+	 delay(2000);
+}
+void loop()
+{
+	 flag=0;
+	 if(count==0){
+		 int value1=analogRead(x);
+		 int value2=analogRead(y);
+		 int value3=analogRead(z);
+		 int xValue=xsample-value1;
+		 int yValue=ysample-value2;
+		 int zValue=zsample-value3;
+		 Serial.println(xValue);
+		 Serial.println(yValue);
+		 Serial.println(zValue);
+		 //calculating pulses in the last few seconds (to check for a surge)
+		 if (millis()<twosec+2000)
+		 {
+			 if(analogRead(data)<100){
+				 pulse_count++;
+				 while(analogRead(data)<100);
+			 }
+		 }
+		 else{
+			 twosec=millis(); //refresh that variable every 2 sec
+			 Serial.println("No. of pulses in last 2 sec=" + String(pulse_count));
+			 pulse_count=0;
+		 }
+		 if((pulse_count>4)&&(zValue<MaxVal)) //accident detected (pulse surges to around 120 BPM and vehicle undergoes a fall)
+		 {
+			speaker_with_button();
+		 }
+	 }
+}
+void speaker_with_button()
+{
+	 while(waitcount<15 && flag==0) // 15 seconds waiting time for the user to cancel the accident alert
+	 {
+		 tone( 8, 33, 500); //buzzing sound issued to alert the user
+		 delay(1000);
+		 waitcount=waitcount+1;
+		 buttonstate = digitalRead(start);
+		 if(buttonstate==1)
+		 {
+			flag=1;
+		 }
+	 }
+	 if(flag==1)
+	 {
+		Serial.println("No accident");
+	 }
+
+	 else if(flag==0) //not cancelled - location fetched and accident alert to be sent
+	 {
+		gpsEvent();
+	 }
+}
+void ShowSerialData() //function to write data to GSM stream
+{
+	 while(GSM_serial.available()!=0)
+		Serial.write(GSM_serial.read());
+	 delay(5000);
+
+}
+void gpsEvent()
+{
+	 while(GPS_serial.available())
+	 {
+		gps.encode(GPS_serial.read());
+	 }
+	 if(gps.location.isUpdated())
+	 {
+		 float latt=gps.location.lat();
+		 float longg=gps.location.lng();
+		 String latitude=String(latt,6);
+		 String longitude=String(longg,6);
+		 if (GSM_serial.available())
+		 {
+			 Serial.write(GSM_serial.read());
+			 //GSM commands to initiate communication with cloud
+			 GSM_serial.println("AT");
+			 delay(1000);
+			 GSM_serial.println("AT+CPIN?");
+			 delay(1000);
+			 GSM_serial.println("AT+CREG?");
+			 delay(1000);
+			 GSM_serial.println("AT+CGATT?");
+			 delay(1000);
+			 GSM_serial.println("AT+CIPSHUT");
+			 delay(1000);
+			 GSM_serial.println("AT+CIPSTATUS");
+			 delay(2000);
+			 GSM_serial.println("AT+CIPMUX=0");
+			 delay(2000);
+			 ShowSerialData();
+			 GSM_serial.println("AT+CSTT=\"airtelgprs.com\"");//start task and set the APN
+			 delay(1000);
+			 ShowSerialData();
+			 GSM_serial.println("AT+CIICR");//bring up wireless connection
+			 delay(3000);
+			 ShowSerialData();
+			 GSM_serial.println("AT+CIFSR");//get local IP adress
+			 delay(2000);
+			 ShowSerialData();
+			 GSM_serial.println("AT+CIPSPRT=0");
+			 delay(3000);
+			 ShowSerialData();
+
+			 GSM_serial.println("AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",\"80\"");//start up the connection	
+			 delay(6000);
+			 ShowSerialData();
+			 GSM_serial.println("AT+CIPSEND");//begin send data to remote server
+			 delay(4000);
+			 ShowSerialData();
+
+			 String str="GET https://api.thingspeak.com/update?
+			 api_key=08FY5SR8KCQVNZN1&field4=" + latitude +"&field5="+longitude;
+			 Serial.println(str);
+			 GSM_serial.println(str);//begin send data to remote server
+
+			 delay(4000);
+			 ShowSerialData();
+			 GSM_serial.println((char)26);//sending
+			 delay(5000);//waiting for reply -important. time depends on the quality of network connection
+			 GSM_serial.println();
+			 ShowSerialData();
+			 GSM_serial.println("AT+CIPSHUT");//close the connection
+			 delay(100);
+			 ShowSerialData();
+		 }
+		 while(count==0)
+		 {
+
+			 //SMS message sent to family member or friend (optional)
+			 GSM_serial.println("AT+CMGF=1");
+			 delay(1000);
+			 GSM_serial.println("AT+CMGS=\"+918310826023\"\r"); //mobile number
+			 delay(1000);
+			 GSM_serial.println("Accident has been detected at the following location. Alert has been sent to nearby hospital.");
+			 GSM_serial.println("Latitude:");
+			 GSM_serial.println(gps.location.lat(), 6);
+			 GSM_serial.println("Longitude:");
+			 GSM_serial.println(gps.location.lng(), 6);
+			 delay(100);
+			 GSM_serial.println((char)26);
+			 delay(1000);
+			count++; /*count variable is used so that once an accident has occurred and necessary data has been sent, system stops further monitoring and
+						alerting. */
+
+		 }
+	 }
+}
---
+++ software.py
@@ -0,0 +1,123 @@
+import urllib.request  as urllib2
+import json
+import time
+import requests
+from flask import Flask, redirect, render_template, request, session, url_for
+from googleplaces import GooglePlaces, types, lang 
+from twilio.rest import Client
+import smtplib 
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders
+from math import *
+from decimal import Decimal
+import pymysql
+import email
+import imaplib
+
+#function to find the distance between two coordinate points
+def haversine_distance(lat2,long2,lat1,long1):
+    slat=radians(lat1)
+    slon=radians(long1)
+    elat=radians(lat2)
+    elon=radians(long2)
+    dist = 6371.0710  * acos(sin(slat)*sin(elat) + cos(slat)*cos(elat)*cos(slon - elon))
+    return dist
+
+#function for database connectivity
+def database_fetch(i,lat1,long1):
+    connection = pymysql.connect(host="localhost",user="root",passwd="",database="miniproject_hospital" )
+    cursor = connection.cursor()
+
+    #executing the quires
+    cursor.execute('Select * from hospitaldetails where name = %s',i)
+    rows = cursor.fetchall()
+    for row in rows:
+        print('Hospital Name : ',i)
+        print('Phone Number : ',row[1])
+        print('EmailID : ',row[2])
+        phno=row[1]
+        voice_call(phno)
+        print(phno)
+        ID=row[2]
+        mail(ID,lat1,long1)
+    connection.close()
+    
+#function to call the nearest hospital
+def voice_call(a):
+    account_sid = 'XXXXXXXXX' #add account sid from twilio account
+    auth_token = 'YYYYYYYYYY' #add account token from twilio account
+    client = Client(account_sid, auth_token)
+
+    call = client.calls.create(
+                        twiml='<Response ><Say voice="alice">Emergency....Accident has been detected near to your hospital Check your official mail for location Thank you</Say></Response>',
+                        to='+91'+a, #hospital phone number
+                        from_='' #twilio number      
+               )    
+    print('Call SID number : ',call.sid)
+
+#function to send mail
+def mail(ID,lat1,long1):
+    fromaddr = "xx@gmail.com" #sender's mail address
+    toaddr = ID
+    msg = MIMEMultipart() 
+    msg['From'] = fromaddr 
+    msg['To'] = toaddr 
+    msg['Subject'] = "Accident location"
+    url="http://www.google.com/maps/place/"+str(lat1)+","+str(long1)
+    body = "Latitude : "+str(lat1)+"\nLongitude : "+str(long1)+"\nClick on the link for directions\n"+url
+    msg.attach(MIMEText(body, 'plain')) 
+    s = smtplib.SMTP('smtp.gmail.com', 587) 
+    s.starttls() 
+    s.login(fromaddr,"****") #sender's gmail password
+    text = msg.as_string() 
+    s.sendmail(fromaddr, toaddr, text)  
+    print('Email sent')
+    s.quit() 
+
+#function to locate nearby hospitals and find nearest among those
+def map(lat1,long1):
+    API_KEY = 'xxxxxx' #google cloud api key for maps
+    google_places = GooglePlaces(API_KEY) 
+    query_result = google_places.nearby_search(  
+    lat_lng ={'lat':lat1 , 'lng':long1 }, 
+    radius = 500, 
+    types =[types.TYPE_HOSPITAL]) 
+    if query_result.has_attributions: 
+        print (query_result.html_attributions) 
+    dict1={}
+    for place in query_result.places: 
+        s=""
+        if 'Hospital'.lower() in place.name.lower() and 'diagnostics'.lower() not in place.name.lower() and 'dental'.lower() not in place.name.lower() and 'eye'.lower() not in place.name.lower(): 
+            s=place.name
+            distance=haversine_distance(place.geo_location['lat'],place.geo_location['lng'],lat1,long1)
+            dict1[s]=distance
+    print(dict1)
+    val = dict1.values()
+    minval = min(val)
+    for i in dict1:
+        if dict1[i]== minval:
+            j=i
+    database_fetch(j,lat1,long1)
+
+
+#code to fetch coordinates from cloud
+READ_API_KEY='yyyyy' #thingspeak api key
+CHANNEL_ID= 'qqqqqq' #thingspeak channel id 
+lon=0
+lat=0
+while(True):
+    conn = urllib2.urlopen("http://api.thingspeak.com/channels/%s/feeds/last.json?api_key=%s" \
+                                % (CHANNEL_ID,READ_API_KEY))
+    response = conn.read()
+    data=json.loads(response)
+    # print(data)
+    lat1=float(data['field4'])
+    long1=float(data['field5'])
+    if lat1!=lat and long1!=lon:
+        map(lat1,long1)
+        lat=lat1
+        lon=long1
+    conn.close()
+    
